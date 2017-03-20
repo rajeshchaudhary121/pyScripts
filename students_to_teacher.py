f0ec5606f8731e488f0adb9947d6d0c1a31fc96e
rajeshchaudhary121 <rajeshchaudhary121989@gmail.com>
2017-03-20 14:00:50 +0530

    Creating dictionaries for Student to Teacher

---
+++ students_to_teacher.py
@@ -0,0 +1,68 @@
+#creating Dictionaries
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+
+# defining functions
+def average(numbers):
+    total = sum(numbers)
+    total = float(total)
+    average = total / len(numbers)
+    return average
+
+
+def get_average(student):
+    homework = average(student["homework"]) * 0.1
+    quizzes = average(student["quizzes"]) * 0.3
+    tests = average(student["tests"]) * 0.6
+
+    # homework * 0.1
+    # quizzes * 0.3
+    # tests * 0.6
+
+    return (homework + quizzes + tests)
+
+# calculating Grades
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+
+
+letter_grade = get_letter_grade(get_average(lloyd))
+print(letter_grade)
+
+# calculating average
+def class_average(students):
+    results = []
+    for student in students:
+        results.append(get_average(student))
+    return average(results)
+
+#printing the desired output
+print(class_average([lloyd, alice, tyler]))
+print(get_letter_grade(class_average([lloyd, alice, tyler])))
+
