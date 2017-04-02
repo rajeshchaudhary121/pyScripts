0af8818040a0bffa8ab0c4ed5d6da32c9d150352
rajeshchaudhary121 <rajeshchaudhary121989@gmail.com>
2017-04-02 09:00:15 +0530

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -1,35 +1,21 @@
 from random import randint
-
 from Tools.scripts.treesync import raw_input
-
 board = []
-
 for x in range(5):
     board.append(["O"] * 5)
-
-
 def print_board(board):
     for row in board:
         print(" ".join(row))
-
-
 print("Let's play Battleship!")
 print_board(board)
-
-
 def random_row(board):
     return randint(0, len(board) - 1)
-
-
 def random_col(board):
     return randint(0, len(board[0]) - 1)
-
-
 ship_row = random_row(board)
 ship_col = random_col(board)
 print(ship_row)
 print(ship_col)
-
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
 for turn in range(4):
